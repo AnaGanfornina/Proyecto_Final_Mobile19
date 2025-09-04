11dd48660bc1832424a44cf5ef6c64c819ac36e8
Ana Ganfornina Arques <ana@Mac-mini-de-Ana.local>
2025-09-04 17:39:34 +0200

    build: create base Xcode project setup

---
+++ FitTrack_iOS/FitTrack_iOS/FitTrack_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrack_iOSApp.swift
+//  FitTrack_iOS
+//
+//  Created by Ana Ganfornina Arques on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FitTrack_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

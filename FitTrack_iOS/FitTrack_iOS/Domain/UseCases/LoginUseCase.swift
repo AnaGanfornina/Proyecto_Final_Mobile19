93cdaa1a2e7ad78c50f6d965a193394583177c28
Ana Ganfornina Arques <ana@Mac-mini-de-Ana.local>
2025-09-09 16:20:33 +0200

    feat: LoginUserCase logic with login repository and ApiProviderMock

---
+++ FitTrack_iOS/FitTrack_iOS/Domain/UseCases/LoginUseCase.swift
@@ -0,0 +1,52 @@
+//
+//  LoginUserCase.swift
+//  FitTrack_iOS
+//
+//  Created by Ana Ganfornina Arques on 7/9/25.
+//
+
+import Foundation
+
+protocol LoginUseCaseProtocol {
+    func login(user: String, password: String)  async throws -> Bool
+}
+
+final class LoginUseCase: LoginUseCaseProtocol {
+    
+    var repository: any LoginRepositoryProtocol
+    
+    
+    init(repository: any LoginRepositoryProtocol = LoginRepository(network: ApiProvider())) {
+        self.repository = repository
+    }
+    
+    
+    func login(user: String, password: String) async throws -> Bool {
+        
+        
+        let token = try await repository.loginApp(user: user, pass: password)
+        return token != ""
+    }
+}
+
+// MARK: -  LoginUseCaseMock
+final class LoginUseCaseMock: LoginUseCaseProtocol {
+    
+    var repository: any LoginRepositoryProtocol
+    
+    
+    init(repository: any LoginRepositoryProtocol = LoginRepository(network: ApiProviderMock())) {
+        self.repository = repository
+    }
+    
+    
+    func login(user: String, password: String) async throws -> Bool {
+        let token = try await repository.loginApp(user: user, pass: password)
+        return token == "tokenMock"
+    }
+    
+    
+    
+    
+    
+}

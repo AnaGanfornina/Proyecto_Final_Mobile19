3c738b586b717bbba41255c17cfdf98fbaa06dcd
Ariana Nagumo <arinagumo@gmail.com>
2025-09-13 21:00:56 -0600

    feat(users): create user migration

--- fittrack_server/Sources/fittrack/routes.swift
+++ fittrack_server/Sources/fittrack/routes.swift
@@ -9,7 +9,7 @@ func routes(_ app: Application) throws {
     
     try app.group("api") { builder in
         try builder.register(collection: AuthController())
-        try app.register(collection: UserController())
+        try builder.register(collection: UserController())
         //    try app.register(collection: TrainingController())
         //    try app.register(collection: ExerciseController())
         //    try app.register(collection: AppointmentController())

733661b14c23ee864dea6defed5d3a6acab3ee8a
Ariana Nagumo <arinagumo@gmail.com>
2025-09-19 14:58:12 -0600

    feat(training): create request and dto

---
+++ FitTrack_iOS/FitTrack_iOS/Infraestructure/Networking/Requests/CreateTrainingURLRequest.swift
@@ -0,0 +1,31 @@
+//
+//  CreateTrainingURLRequest.swift
+//  FitTrack_iOS
+//
+//  Created by Ariana Rodríguez on 19/09/25.
+//
+
+import Foundation
+
+struct CreateTrainingURLequest: URLRequestComponents {
+    typealias Response = TrainingDTO
+    var path: String = "/api/trainings"
+    var httpMethod: HTTPMethod = .POST
+    var authorized: Bool = true
+    var body: (any Encodable)?
+    
+    init(name: String, goalId: UUID) {
+        body = Body(
+            name: name,
+            goalId: goalId.uuidString
+        )
+    }
+}
+
+extension CreateTrainingURLequest {
+    struct Body: Encodable {
+        let name: String
+        let goalId: String
+        // TODO: Send exercises data
+    }
+}

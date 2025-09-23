57484e364229a7e56e2294cd9e4279b8952e15e2
Ariana Nagumo <arinagumo@gmail.com>
2025-09-22 19:36:08 -0600

    feat(signup): create repository func and signup use case

--- FitTrack_iOS/FitTrack_iOS/Data/Repository/AuthRepository.swift
+++ FitTrack_iOS/FitTrack_iOS/Data/Repository/AuthRepository.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 protocol AuthRepositoryProtocol {
+    func signup(user: User) async throws
     func login(user: String, password: String) async throws
     func getSession() async throws -> String
 }
@@ -20,6 +21,20 @@ final class AuthRepository: AuthRepositoryProtocol{
          authDatasource: AuthDataSourceProtocol = AuthDataSource.shared) {
         self.apiSession = apiSession
         self.authDataSource = authDatasource
+    }
+    
+    func signup(user: User) async throws {
+        let userDTO = UserDomainToDTOMapper().map(user)
+        let signupData = try await apiSession.request(
+            SignupURLRequest(userDTO: userDTO)
+        )
+        guard let jwt = signupData.accessToken,
+              let userId = signupData.userId else {
+            throw AppError.session("Session not found or invalid, log in again")
+        }
+        // Save userId into UserDefaults to use it to get data related to the main user
+        UserDefaults.standard.set(userId, forKey: "userId")
+        try await authDataSource.set(jwt)
     }
     
     func login(user: String, password: String) async throws {

9e4f47583fdf86397457f17cf1a7d3cf8e44e5ba
Ana Ganfornina Arques <ana@Mac-mini-de-Ana.local>
2025-09-25 23:27:28 +0200

    wip: nuevo cliente with  purple background and dividers

--- FitTrack_iOS/FitTrack_iOS/Presentation/Views/Clients/CreateClientView.swift
+++ FitTrack_iOS/FitTrack_iOS/Presentation/Views/Clients/CreateClientView.swift
@@ -28,118 +28,141 @@ struct CreateClientView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 16) {
-                //Group -> We dont use Form because it needs to be customized
-                Text("Datos Personales")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text("Nombre")
-                    .modifier(CustomTextStyle())
-                TextField("Juan", text: $nombre)
-                    .modifier(CustomTextFieldStyle())
+            ZStack {
                 
-                Text("Apellido")
-                    .modifier(CustomTextStyle())
-                TextField("Hernández", text: $apellido)
-                    .modifier(CustomTextFieldStyle())
+                LinearGradient(gradient: Gradient(
+                    colors: [Color.purple2,Color.purple2.opacity(0.7)]),
+                               startPoint: .top,
+                               endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all) // Fill Full Screen
                 
-                Text("Altura")
-                    .modifier(CustomTextStyle())
-                TextField("1.70", text: $altura)
-                    .modifier(CustomTextFieldStyle())
-                
-                Text("Peso")
-                    .modifier(CustomTextStyle())
-                TextField("70 kg", text: $peso)
-                    .modifier(CustomTextFieldStyle())
-                
-                Text("Objetivo")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                TextEditor(text: $objetivo)
-                    .frame(minHeight: 80) // Min Height
-                    .modifier(CustomTextFieldStyle())
-                
-                Text("Historia y Nivel")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                TextEditor(text: $historia)
-                    .frame(minHeight: 80) // Min Height
-                    .modifier(CustomTextFieldStyle())
-                
-                Text("Mediciones Iniciales")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text("Circunferencia del brazo")
-                    .modifier(CustomTextStyle())
-                TextField("35 cm", text: $circBrazo)
-                    .modifier(CustomTextFieldStyle())
-
-                Text("Circunferencia del abdomen")
-                    .modifier(CustomTextStyle())
-                TextField("78 cm", text: $circAbdomen)
-                    .modifier(CustomTextFieldStyle())
-
-                Text("Circunferencia del muslo")
-                    .modifier(CustomTextStyle())
-                TextField("78 cm", text: $circMuslo)
-                    .modifier(CustomTextFieldStyle())
-
-                Text("Circunferencia del pecho")
-                    .modifier(CustomTextStyle())
-                TextField("78 cm", text: $circPecho)
-                    .modifier(CustomTextFieldStyle())
-
-                
-                
-
-                
-
-
-
-                
-                Spacer()
-            }// VStack
-            .padding()
-            .navigationBarBackButtonHidden(true) /// Hide `< Back` button
-            .toolbar {
-                // Tool Bar at the Top
-                ToolbarItem(placement: .navigationBarLeading) {
-                    HStack(spacing: 80) {
-                        // Chevron to go back Home
-                        Button {
-                            dismiss()
-                            isTabBarHidden = false // Show tab bar when going back
-                        } label: {
-                            Image(systemName: "chevron.left")
-                                .font(.system(size: 18, weight: .semibold))
-                                .foregroundColor(.purple)
-                        }
-                        
-                        // Texto junto al chevron
-                        Text("Nuevo Cliente")
-                            .foregroundColor(.black)
-                            .fontWeight(.bold)
-                            .font(.title2)
-                            .frame(minWidth: 150)
-                        
-                        // Title Text
-                        Button {
-                            // TODO: Save the created client action
-                            dismiss()
-                            isTabBarHidden = false // Show tab bar when going back
-                        } label: {
-                            Text("OK")
-                                .foregroundColor(.purple)
+                VStack(spacing: 16) {
+                    
+                    //Group -> We dont use Form because it needs to be customized
+                    Text("Datos Personales")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Divider()
+                        .background(Color.white)
+                    Text("Nombre")
+                        .modifier(CustomTextStyle())
+                    TextField("Juan", text: $nombre)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Apellido")
+                        .modifier(CustomTextStyle())
+                    TextField("Hernández", text: $apellido)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Altura")
+                        .foregroundColor(.white)
+                        .modifier(CustomTextStyle())
+                    TextField("1.70", text: $altura)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Peso")
+                        .modifier(CustomTextStyle())
+                    TextField("70 kg", text: $peso)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Objetivo")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Divider()
+                        .background(Color.white)
+                    
+                    TextEditor(text: $objetivo)
+                        .frame(minHeight: 80) // Min Height
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Historia y Nivel")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Divider()
+                        .background(Color.white)
+                    
+                    TextEditor(text: $historia)
+                        .frame(minHeight: 80) // Min Height
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Mediciones Iniciales")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Divider()
+                        .background(Color.white)
+                    
+                    Text("Circunferencia del brazo")
+                        .modifier(CustomTextStyle())
+                    TextField("35 cm", text: $circBrazo)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Circunferencia del abdomen")
+                        .modifier(CustomTextStyle())
+                    TextField("78 cm", text: $circAbdomen)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Circunferencia del muslo")
+                        .modifier(CustomTextStyle())
+                    TextField("78 cm", text: $circMuslo)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    Text("Circunferencia del pecho")
+                        .modifier(CustomTextStyle())
+                    TextField("78 cm", text: $circPecho)
+                        .modifier(CustomTextFieldStyle())
+                    
+                    
+                    
+ 
+                    Spacer()
+                }// VStack
+                .padding()
+                .navigationBarBackButtonHidden(true) /// Hide `< Back` button
+                .toolbar {
+                    // Tool Bar at the Top
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        HStack(spacing: 80) {
+                            // Chevron to go back Home
+                            Button {
+                                dismiss()
+                                isTabBarHidden = false // Show tab bar when going back
+                            } label: {
+                                Image(systemName: "chevron.left")
+                                    .font(.system(size: 18, weight: .semibold))
+                                    .foregroundColor(.purple)
+                            }
+                            
+                            // Texto junto al chevron
+                            Text("Nuevo Cliente")
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
                                 .font(.title2)
-                                .frame(minWidth: 10)
+                                .frame(minWidth: 150)
+                            
+                            // Title Text
+                            Button {
+                                // TODO: Save the created client action
+                                dismiss()
+                                isTabBarHidden = false // Show tab bar when going back
+                            } label: {
+                                Text("OK")
+                                    .foregroundColor(.purple)
+                                    .font(.title2)
+                                    .frame(minWidth: 10)
+                            }
                         }
-                    }
-                } // Toolbar
+                    } // Toolbar
+                }
             }
         } // ScrllView
     }
@@ -163,6 +186,7 @@ struct CustomTextFieldStyle: ViewModifier {
 struct CustomTextStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
+            .foregroundColor(.white)
             .font(.body)
             .frame(maxWidth: .infinity, alignment: .leading)
     }
